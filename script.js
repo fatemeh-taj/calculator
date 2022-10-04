664ad599f89f063c10ef32c66eb8664be20e0dd8
taj <ftem.taj@gmail.com>
2022-10-05 03:18:58 +0330

    better coding

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 "use strict";
 
 let root = document.querySelector("html");
-let btnmenu = document.querySelector(".menu");
 
-btnmenu.addEventListener("click", function () {
-
+document.querySelector("#menu").addEventListener("click", () => {
+    
     switch (root.getAttribute("data-theme")) {
         case "default":
             root.setAttribute("data-theme", "green");
             break;
+        
         case "green":
             root.setAttribute("data-theme", "blue");
             break;
+        
         case "blue":
             root.setAttribute("data-theme", "pink");
             break;
+        
         case "pink":
             root.setAttribute("data-theme", "default");
             break;
